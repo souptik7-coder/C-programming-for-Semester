d27521910a6393659598d1452e7cd3b686760738
Souptik Koley <souptikmemari264@gmail.com>
2026-06-15 03:07:07 +0530

    pattern

---
+++ pattern/letterpattern1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 1; i <= 7; i++) {
+
+        char ch = 'A';
+
+        for (int j = 1; j <= i; j++) {
+            printf("%c ", ch);
+            ch++;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
