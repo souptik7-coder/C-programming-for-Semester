045ba78d0b4acad8f14a8729e69e56049d29f90b
Souptik Koley <souptikmemari264@gmail.com>
2026-06-15 19:17:04 +0530

    enternumber

--- Loop/enterallnumexceptmultiple10.c
+++ Loop/enterallnumexceptmultiple10.c
@@ -1,22 +1,20 @@
-import java.util.*;
-public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        
-        // display all numbers entered except multiple of 10
-        
-        while (true) {
-            
-            System.out.println("Enter your number :");
-            int n = sc.nextInt();
-            
-            if (n % 10 == 0) {
-                continue;
-            }
-            
-            System.out.println("The number is : " + n );
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Display all numbers entered except multiples of 10
+
+    while (1) {
+        printf("Enter your number: ");
+        scanf("%d", &n);
+
+        if (n % 10 == 0) {
+            continue;
         }
-      
-        
+
+        printf("The number is: %d\n", n);
     }
+
+    return 0;
 }
